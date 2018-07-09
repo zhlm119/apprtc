74d88bf59f9fefc45f2a97e1cf156d0d325361d8
zhonglm <zhonglm@wondershare.cn>
2018-07-09 13:47:31 +0800

    IP Changed

--- src/app_engine/constants.py
+++ src/app_engine/constants.py
@@ -38,7 +38,7 @@ ICE_SERVER_OVERRIDE = None
 #   }
 # ]
 
-ICE_SERVER_BASE_URL = 'http://www.aspy.gq'
+ICE_SERVER_BASE_URL = 'https://aspy.gq'
 ICE_SERVER_URL_TEMPLATE = '%s/iceconfig?key=%s'
 ICE_SERVER_API_KEY = os.environ.get('ICE_SERVER_API_KEY')
 
